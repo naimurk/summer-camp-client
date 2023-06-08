0f2f3d00775782945817978d1922398bafd36206
naimurk <naimurk781@gmail.com>
2023-06-09 00:26:07 +0600

    avaiable seats update function done

--- src/Hooks/useClasses.jsx
+++ src/Hooks/useClasses.jsx
@@ -2,14 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 
 
 const useClasses = () => {
-    const {data : classes = [] } = useQuery({
+    const {data : classes = [],refetch } = useQuery({
         queryKey : ['classes'],
         queryFn : async () => {
             const response = await fetch('http://localhost:5000/classes')
             return response.json()
         }
     })
-    return [classes]
+    return [classes,refetch]
       
 };
 
--- src/Layout/DashBoard/StudentDashBoard/Checkout/Checkout.jsx
+++ src/Layout/DashBoard/StudentDashBoard/Checkout/Checkout.jsx
@@ -5,41 +5,42 @@ import './Checkout.css'
 import Swal from "sweetalert2";
 import useCarts from "../../../../Hooks/useCarts";
 import { AuthContext } from "../../../../AuthProvider/AuthProvider";
+import useClasses from "../../../../Hooks/useClasses";
 
-const Checkout = ({item}) => {
-   
+const Checkout = ({ item }) => {
+
     console.log(item);
-    const {ClassId,_id,image, email,instructor_name,price, available_seats,classname } = item
-
+    const { ClassId, _id, image, email, instructor_name, price, available_seats, classname } = item
+    // const [refetch] = useClasses()
     const stripe = useStripe();
     const elements = useElements();
-    const {user,loading } = useContext(AuthContext)
+    const { user, loading } = useContext(AuthContext)
     const [errorP, setErrorP] = useState('')
-    const [carts,refetch] = useCarts();
+    const [carts, refetch] = useCarts();
     console.log(carts);
     const [clientSecret, setClientSecret] = useState("");
-    const [processing , setProcessing] = useState(false)
+    const [processing, setProcessing] = useState(false)
     // const total = user && carts?.reduce((sum, item) => sum + item.price, 0);
     // const price = parseFloat(total?.toFixed(2));
-    const [message , setMessage] = useState('')
+    const [message, setMessage] = useState('')
     // console.log(price);
     const token = localStorage.getItem('access-token')
 
     useEffect(() => {
-        if(price > 0){
+        if (price > 0) {
             fetch('http://localhost:5000/create-payment-intent', {
                 method: 'POST',
                 headers: {
-                  'content-type': 'application/json',
-                  authorization : `bearer ${token}`
+                    'content-type': 'application/json',
+                    authorization: `bearer ${token}`
                 },
-                body: JSON.stringify({price})
-              })
+                body: JSON.stringify({ price })
+            })
                 .then(res => res.json())
                 .then(data => setClientSecret(data.clientSecret))
                 .catch(error => console.error(error));
         }
-      }, [price,loading]);
+    }, [price, loading]);
 
 
 
@@ -51,7 +52,7 @@ const Checkout = ({item}) => {
 
 
 
-     
+
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -60,7 +61,7 @@ const Checkout = ({item}) => {
 
             return;
         }
-        
+
 
 
         setProcessing(true)
@@ -71,12 +72,12 @@ const Checkout = ({item}) => {
         }
 
         if (!clientSecret) {
-            
+
             console.error("Invalid client secret");
             return;
         }
         console.log("Client Secret:", clientSecret);
-        
+
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card,
@@ -90,73 +91,86 @@ const Checkout = ({item}) => {
         }
 
 
-        const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
             clientSecret,
             {
-              payment_method: {
-                card: card,
-                billing_details: {
-                  name: user?.name,
-                  email : user?.email
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: user?.name,
+                        email: user?.email
 
+                    },
                 },
-              },
             },
-          );
-          setProcessing(false)
+        );
+        setProcessing(false)
 
-          if(confirmError){
+        if (confirmError) {
             console.log(confirmError);
-          }
-        console.log( 'payment intent',paymentIntent);
-        
-       if(paymentIntent?.status === 'succeeded'){
-        setProcessing(true)
-        
-        setMessage(`thank for purchasing. your transaction Id : ${paymentIntent.id}`)
-        
-        // alert('congratulation')
-        const payment = { 
-            name : user?.name,
-            email : user?.email,
-            data : new Date(),
-            transaction: paymentIntent?.id,
-            // total_item : carts?.length  ,
-            // itemNames : user && carts?.map(item => item.name),
-            className : classname,
-            itemId : _id,
-            // itemId : user && carts?.map(item => item._id),
-            // item : user && carts?.map(item => item),
-            ClassId : ClassId,
-            instructor_name : instructor_name,
-            available_seats : available_seats,
-            image : image,
-            price : price
-            
-            
         }
+        console.log('payment intent', paymentIntent);
 
-        // console.log(payment.item);
+        if (paymentIntent?.status === 'succeeded') {
+            setProcessing(true)
 
-       
+            setMessage(`thank for purchasing. your transaction Id : ${paymentIntent.id}`)
 
-        fetch('http://localhost:5000/payment', {
-            method : "POST", 
-            headers : {
-                'content-type' : 'application/json'
-            },
-            body : JSON.stringify(payment)
-        })
-        .then(res => res.json())
-        .then(data => {
-            if(data.result.insertedId ){
-                refetch();
-                
+            // alert('congratulation')
+            const payment = {
+                name: user?.name,
+                email: user?.email,
+                data: new Date(),
+                transaction: paymentIntent?.id,
+                // total_item : carts?.length  ,
+                // itemNames : user && carts?.map(item => item.name),
+                className: classname,
+                itemId: _id,
+                // itemId : user && carts?.map(item => item._id),
+                // item : user && carts?.map(item => item),
+                ClassId: ClassId,
+                instructor_name: instructor_name,
+                available_seats: available_seats,
+                image: image,
+                price: price
+
+
             }
-            
-        })
-        
-       }
+
+            // console.log(payment.item);
+
+
+
+            fetch('http://localhost:5000/payment', {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(payment)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.result.insertedId) {
+                        refetch();
+                        fetch(`http://localhost:5000/classes/update/${ClassId}`, {
+                            method: 'PATCH',
+                            headers : {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify({available_seats})
+                            
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                console.log(data);
+                               
+                            })
+
+                    }
+
+                })
+
+        }
 
     }
     return (
